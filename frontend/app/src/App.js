bc5b3cd060d580974ae9bdc360ff2b249e00f7e6
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-04 15:38:30 +0200

    adding addTodo

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
 
   const fetchTodos = () => {
     fetch('/todos')
@@ -11,13 +12,44 @@ function App() {
       .then((resJson) => setTodos(resJson));
   };
 
+  const addTodo = (id) => {
+    const bodyObject = {
+      id: id,
+      desc: input,
+    };
+    /* console.log(bodyObject); */
+    fetch('/addTodo', {
+      method: 'POST',
+      body: JSON.stringify(bodyObject),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((resJson) => console.log(resJson));
+  };
+
   useEffect(fetchTodos, []);
 
   return (
     <div className='App'>
-      {todos.map((todo) => (
-        <h2>{todo.desc}</h2>
+      {todos.map((todo, index) => (
+        <h2 key={index}>
+          id:{todo.id}
+          <br />
+          {todo.desc}
+        </h2>
       ))}
+
+      <input
+        type='text'
+        value={input}
+        placeholder='enter description'
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(todos[todos.length - 1].id + 1)}>
+        add todo
+      </button>
     </div>
   );
 }
