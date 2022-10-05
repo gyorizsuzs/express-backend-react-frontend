0c50e088045035b50cbf2b7362b7c42b1c45cf55
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-05 14:40:12 +0200

    loading mask

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -5,11 +5,16 @@ import './App.css';
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(true);
 
   const fetchTodos = () => {
+    setLoading(true);
     fetch('/todos')
       .then((res) => res.json())
-      .then((resJson) => setTodos(resJson));
+      .then((resJson) => {
+        setLoading(false);
+        setTodos(resJson);
+      });
   };
 
   const addTodo = (id) => {
@@ -37,22 +42,30 @@ function App() {
 
   return (
     <div className='App'>
-      <input
-        type='text'
-        value={input}
-        placeholder='enter description'
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={() => addTodo(todos[todos.length - 1].id + 1)}>
-        add todo
-      </button>
-      {todos.map((todo, index) => (
-        <h2 key={index}>
-          id:{todo.id}
-          <br />
-          {todo.desc}
-        </h2>
-      ))}
+      {loading ? (
+        <div className='loading'>
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <div>
+          <input
+            type='text'
+            value={input}
+            placeholder='enter description'
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={() => addTodo(todos[todos.length - 1].id + 1)}>
+            add todo
+          </button>
+          {todos.map((todo, index) => (
+            <h2 key={index}>
+              id:{todo.id}
+              <br />
+              {todo.desc}
+            </h2>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
